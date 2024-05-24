0085e9cb03e49fae3ca5498f24da7eda4dae192c
sujansijakhow <sujansijakhow@gmail.com>
2024-05-24 07:40:51 +0545

    routessetup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "./routes/Home";
+import Menu from "./routes/Menu";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import Aboutus from "./routes/Aboutus";
+import Allergy from "./routes/Allergy";
 
-function App () {
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/menu",
+      element : <Menu />
 
+    },
+    {
+      path: "/service",
+      element: <Service />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    },
+    {
+      path: "/aboutus",
+      element: <Aboutus />
+    },
+    {
+      path: "/allergy",
+      element: <Allergy />
+    }
+  ])
+  
+  return (
+    <RouterProvider router={router}>
+      <div className="app">
+        hero
+        
+      </div>
 
-  return(
-    <div className="bg-slate-900">
-      djf
-    </div>
+    </RouterProvider>
   )
 }
 
-export default App
+export default App;
