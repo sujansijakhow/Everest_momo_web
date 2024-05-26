f8fa4ccc36f0e1906a86abe86f9971e6272b9b21
sujansijakhow <sujansijakhow@gmail.com>
2024-05-26 22:14:13 +0545

    home UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import Logo from "../images/Logo.png";
 
 function Navbar() {
     return (
-        <div className="bg-white border b-1 w-full h-16 flex items-center justify-evenly z-20 relative">
+        <div className="bg-white border-b w-full h-16 flex items-center justify-evenly fixed top-0 left-0 z-50">
             <div className="flex">
                 <img src={Logo} alt="" width={85} />
                 <ul className="flex ml-12 space-x-8 text-sm text-gray-700">
@@ -20,8 +20,8 @@ function Navbar() {
             <div className="flex items-center space-x-6">
                 <div className="flex space-x-5">
                     <TiSocialFacebook style={{ height: "20px", width: "20px", backgroundColor: "#A6AEBB", color: "white", borderRadius: "50%" }} />
-                    <RiTiktokFill style={{ height: "20px", width: "20px", backgroundColor: "#A6AEBB", color: "white", borderRadius: "50%" }}/>
-                    <SlSocialInstagram style={{ height: "20px", width: "20px", backgroundColor: "#A6AEBB", color: "white", borderRadius: "50%" }}/>
+                    <RiTiktokFill style={{ height: "20px", width: "20px", backgroundColor: "#A6AEBB", color: "white", borderRadius: "50%" }} />
+                    <SlSocialInstagram style={{ height: "20px", width: "20px", backgroundColor: "#A6AEBB", color: "white", borderRadius: "50%" }} />
                 </div>
                 <Button label={"Contact Us"} color={"color-primary"} />
             </div>
